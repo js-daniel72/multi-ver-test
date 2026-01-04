10d5957d104e7595e4aab6c5fdfb07d6beb9ea25
Daniel Jiesung Jung <daniel72@postech.ac.kr>
2026-01-04 15:35:26 +0000

    version 1

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <limits.h>
+ 
+int main(void)
+{
+    unsigned int input;
+    read(0, &input, 4);
+
+    if(input < UINT_MAX >> 1)
+    {
+        raise(SIGSEGV);
+    }
+    return 0;
+}
